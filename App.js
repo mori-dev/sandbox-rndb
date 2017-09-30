16b9f369c17b27c680752fb9f39cce4016b8ac3b
mori-dev <mori.dev.asdf@gmail.com>
2017-09-30 15:13:07 +0900

    feat: add point system

--- App.js
+++ App.js
@@ -1,23 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Root, Container, Text, View, Button } from 'native-base';
+
+import styles from "./styles";
+
+import * as Expo from 'expo';
 
 export default class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      readyFlag: false,
+      point: 0,
+    };
+  }
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Ionicons': require('@expo/vector-icons/fonts/Ionicons.ttf'),
+    });
+    this.setState({ readyFlag: true });
+  }
+
+  add() {
+    this.setState({ point: (this.state.point + 1)});
+  }
+
   render() {
+    if (!this.state.readyFlag) {
+      return <Expo.AppLoading />;
+    }
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Root>
+        <Container style={styles.container}>
+          <View>
+            <Text>{this.state.point}</Text>
+            <Text>{this.state.point}</Text>
+            <Text>{this.state.point}</Text>
+            <Text>{this.state.point}</Text>
+            <Text>{this.state.point}</Text>
+            <Text>{this.state.point}</Text>
+            <Text>{this.state.point}</Text>
+          </View>
+          <View style={styles.buttonContainer}>
+            <Button
+              rounded
+              onPress={this.add.bind(this)}
+              style={{ backgroundColor: "red" }}
+            >
+              <Text>追加</Text>
+            </Button>
+          </View>
+        </Container>
+      </Root>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,19 @@
+export default {
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+    backgroundColor: 'white',
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    flex: 1,
+    position: "absolute",
+    bottom: 50,
+    left: 0,
+    right: 0,
+    justifyContent: "space-between",
+    padding: 15
+  },
+}
